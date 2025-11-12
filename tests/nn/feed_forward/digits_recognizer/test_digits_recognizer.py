799b642bfb67e0a95b30a114b585cbdfc1a39a98
Maciaz456 <maciej.dalek99@proton.me>
2025-11-12 09:33:09 +0100

    Digits tests refactored.

--- tests/nn/feed_forward/digits_recognizer/test_digits_recognizer.py
+++ tests/nn/feed_forward/digits_recognizer/test_digits_recognizer.py
@@ -8,7 +8,7 @@ from nn.ff.digits.d_common import DigitsException
 
 
 @pytest.fixture
-def device():
+def dummy_device():
     return torch.device(
         'cuda' if torch.cuda.is_available() else 'cpu'
     )
@@ -19,13 +19,13 @@ def device():
         [500, 200]
     ]
 )
-def digits(
-    device,
+def dummy_digits(
+    dummy_device,
     request,
     dummy_my_logger
 ):
     return Digits(
-        device,
+        dummy_device,
         request.param,
         dummy_my_logger
     )
@@ -33,76 +33,91 @@ def digits(
 
 @pytest.fixture
 def dummy_tensor():
-    return torch.ones(
-        (28, 28),
-        dtype=torch.float32
-    )
+    def subfunction(
+        seed
+    ):
+        torch.manual_seed(
+            seed
+        )
+        dummy_tensor = torch.rand(
+            (28, 28),
+            dtype=torch.float32
+        )
+
+        return dummy_tensor
+
+    return subfunction
 
 
-@pytest.mark.parametrize(
-    'hidden_sizes',
-    argvalues=[
-        [500],
-        [200, 100]
-    ],
-    ids=[
-        'one_hidden_layer',
-        'two_hidden_layers'
-    ]
+@patch(
+    'nn.ff.digits.digits.torch.nn.ReLU', spec_set=torch.nn.ReLU
+)
+@patch(
+    'nn.ff.digits.digits.torch.nn.Linear', spec_set=torch.nn.Linear
 )
 def test_layers_setup(
-    device,
-    hidden_sizes,
+    linear_mock,
+    relu_mock,
+    dummy_device,
     dummy_my_logger
 ):
-    digits = Digits(
-        device,
+    hidden_sizes = [
+        500,
+        100
+    ]
+
+    Digits(
+        dummy_device,
         hidden_sizes,
         dummy_my_logger
     )
 
-    layers = digits._Digits__layers
-    n_layers = len(
-        layers
+    assert linear_mock.call_args_list == [
+        call(
+            N_PIXELS, 500
+        ),
+        call(
+            500, 100
+        ),
+        call(
+            100, N_CLASSES
+        )
+    ]
+
+    assert len(
+        relu_mock.call_args_list
+    ) == len(
+        hidden_sizes
     )
-    for i in range(
-        n_layers
-    ):
-
-        if i == 0:
-            assert layers[i].in_features == N_PIXELS
-        elif i == len(layers) - 1:
-            assert layers[i].out_features == N_CLASSES
-
-        if i < n_layers - 1:
-            assert layers[i].out_features == layers[i+1].in_features
-
-    assert n_layers == len(hidden_sizes) + 1
 
 
+@patch(
+    'nn.ff.digits.digits.torch.Tensor.to',
+    return_value='to_return'
+)
 def test_forward(
-    digits,
+    to_mock,
+    dummy_digits,
     dummy_tensor
 ):
-    for i, layer in enumerate(
-        digits._Digits__layers
-    ):
-        mocked_layer = Mock(
-            spec_set=layer,
-            return_value=dummy_tensor
-        )
-        digits._Digits__layers[i] = mocked_layer
-
-    digits._relu = Mock(
-        spec_set=digits._relu
+    dummy_digits._Digits__layers.forward = Mock(
+        return_value=dummy_tensor(1)
     )
 
-    digits.forward(
-        dummy_tensor
+    return_value = dummy_digits(
+        dummy_tensor(1)
     )
-    for layer in digits._Digits__layers:
-        layer.assert_called_once()
-    assert digits._relu.call_count == len(digits._Digits__layers) - 1
+
+    to_mock.assert_called_once_with(
+        dummy_digits._Digits__device
+    )
+
+    dummy_digits._Digits__layers.forward.assert_called_once_with(
+        to_mock.return_value
+    )
+    assert (
+        return_value == dummy_digits._Digits__layers.forward.return_value
+    ).all()
 
 
 @patch(
@@ -114,12 +129,12 @@ def test_forward(
 def test_mnist_dataset(
     data_loader_mock,
     mnist_mock,
-    digits,
+    dummy_digits,
     tmp_path
 ):
     batch_size = 100
 
-    digits.prepare_mnist_dataset(
+    dummy_digits.prepare_mnist_dataset(
         tmp_path,
         batch_size=batch_size
     )
@@ -128,24 +143,24 @@ def test_mnist_dataset(
         call(
             root=tmp_path,
             train=True,
-            transform=digits._transform,
+            transform=dummy_digits._transform,
             download=True
         ),
         call(
             root=tmp_path,
             train=False,
-            transform=digits._transform
+            transform=dummy_digits._transform
         )
     ]
 
     assert data_loader_mock.call_args_list == [
         call(
-            dataset=digits._train_dataset,
+            dataset=dummy_digits._train_dataset,
             batch_size=batch_size,
             shuffle=True
         ),
         call(
-            dataset=digits._test_dataset,
+            dataset=dummy_digits._test_dataset,
             batch_size=batch_size,
             shuffle=False
         )
@@ -153,13 +168,13 @@ def test_mnist_dataset(
 
 
 def test_create_image_tensor(
-    digits,
+    dummy_digits,
     data_folder
 ):
     img_path = data_folder.joinpath(
         'correct.png'
     )
-    image_tensor = digits.create_image_tensor(
+    image_tensor = dummy_digits.create_image_tensor(
         img_path
     )
     correct_tensor_file = data_folder.joinpath(
@@ -179,6 +194,6 @@ def test_create_image_tensor(
     with pytest.raises(
         DigitsException
     ):
-        digits.create_image_tensor(
+        dummy_digits.create_image_tensor(
             img_path
         )
